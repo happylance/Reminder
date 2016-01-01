fe6a1ed91756d90ea967f90a81a88aead5e37463
Liang Liu <nlprliu@gmail.com>
2016-01-01 16:14:33 -0500

    Make the message parameter optional.

--- showDialog.sh
+++ showDialog.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -ex
+message="$1"
+[ -z "$1" ] && message="It is $(date +%H:%M) now."
+
 osascript << EOF &>/dev/null
 tell application "Finder"
   activate
-  display dialog "$1"
+  display dialog "$message"
 end tell
 EOF
--- showReminder.sh
+++ showReminder.sh
@@ -2,14 +2,14 @@
 
 _echo_usage () {
     cat << EOF
-usage: $0 [message] [time]
-Example: $0 "It is time to take a break." 3:40pm
+    usage: $0 [time] [message(optional)]
+Example: $0 3:40pm "It is time to take a break."
+         $0 3:40pm
 EOF
     exit 2
 }
 
 [ -z "$1" ] && _echo_usage
-[ -z "$2" ] && _echo_usage
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo $DIR/showDialog.sh \"$1\" | at $2
+echo $DIR/showDialog.sh \"$2\" | at $1
